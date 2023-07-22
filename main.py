61a79ab208bc9bab194c2c21e043df60f768dac7
mouh2020 <badimohammed2019@gmail.com>
2023-07-22 15:38:26 +0100

    pass the unknow errors

--- main.py
+++ main.py
@@ -7,10 +7,14 @@ from handlers.callback import *
 from dependencies.auth import admin_authenticated
 
 @bot.message_handler(func=lambda message:True)
-def all_messages(message : Message) : 
-    if admin_authenticated(message.from_user.id) :
-        print(message.text)
-        message_handler(message=message)
+def all_messages(message : Message) :
+    try : 
+        if admin_authenticated(message.from_user.id) :
+            print(message.text)
+            message_handler(message=message)
+    except Exception as e : 
+        bot.send_message(message.from_user.id,
+                         f"Error occured {str(e)}")
 
 bot.enable_save_next_step_handlers(delay=2)
 bot.load_next_step_handlers()
